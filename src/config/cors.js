4b50c9b0458638cc8995526c9d3ac7f34e169198
João Souza <j.otavioms@gmail.com>
2020-02-20 12:45:00 -0300

    Replace project cors with external middleware

--- src/config/cors.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  res.header('Acess-Control-Allow-Origin', '*');
-  res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Acess-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-}
