7790ba35f5c7dcefc670f510999852a17d97cfe1
João Souza <j.otavioms@gmail.com>
2020-02-20 12:53:52 -0300

    Fix cors call

--- src/config/server.js
+++ src/config/server.js
@@ -1,14 +1,14 @@
 const port = 3003;
 
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const server = express();
-const cors = require('cors');
 const queryParser = require('express-query-int');
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
-server.use(cors);
+server.use(cors());
 server.use(queryParser());
 
 server.listen(port, function() {
