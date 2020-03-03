79e2a14f12ccc1a5d38217b731ccd51ff178f80b
João Souza <j.otavioms@gmail.com>
2020-03-03 18:54:11 -0300

    Refactoring routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const auth = require('./auth');
 
-module.exports = function(server) {
+module.exports = function (server) {
+  // Protected routes by JWT Token
+  const protectedApi = express.Router();
+  server.use('/api', protectedApi);
+  protectedApi.use(auth);
 
-  // Base URL for all routes
-  const router = express.Router();
-  server.use('/api', router);
-
-  // Billing cycle routes
   const BillingCycle = require('../api/billingCycle/billingCycleService');
-  BillingCycle.register(router, '/billingCycles'); 
-}
+  BillingCycle.register(protectedApi, '/billingCycles');
+
+  // Opened routes
+  const openApi = express.Router();
+  server.use('/oapi', openApi);
+
+  const AuthService = require('../api/user/AuthService');
+  openApi.post('/login', AuthService.login);
+  openApi.post('/signup', AuthService.signup);
+  openApi.post('/validateToken', AuthService.validateToken);
+};
